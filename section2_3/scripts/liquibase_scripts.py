919350fab947049d528082e7f57e3bd0e8544c1e
Jeferson Argueta <ajefersonstiv@gmail.com>
2024-12-12 10:17:54 -0600

    Section3 Task1 & Task2 Done
    Section3 Task1 has been added in the docker compose
    Seciont3 Task2 yml files and python script has been relocated into the
    script/

---
+++ section2_3/scripts/liquibase_scripts.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import argparse
+
+def main(params: dict) -> None:
+    """
+        main -> Void
+        @parms  dict    params provided by argparse
+    """
+    command = str(params.execute)
+    docker_meta = {}
+    if command == 'tag':
+        docker_meta['liquibase'] = 'liquibase_tag'
+        docker_meta['docker_file'] = 'docker-compose.liquibase.tag.yml'
+        #
+    elif command == 'update':
+        docker_meta['liquibase'] = 'liquibase_update'
+        docker_meta['docker_file'] = 'docker-compose.liquibase.update.yml'
+        #liquibase_command = os.environ.get('liquibase_update', 'docker-compose -f docker-compose.liquibase.update.yml up')
+    elif command == 'rollback':
+        docker_meta['liquibase'] = 'liquibase_rollback'
+        docker_meta['docker_file'] = 'docker-compose.liquibase.rollback.yml'
+        #liquibase_command = os.environ.get('liquibase_rollback', 'docker-compose -f docker-compose.liquibase.rollback.yml up')
+
+    liquibase_command = os.environ.get(f'{docker_meta["liquibase"]}_up', f'docker-compose -f { docker_meta["docker_file"] } up')
+    liquibase_down = os.environ.get(f'{docker_meta["liquibase"]}_down', f'docker-compose -f { docker_meta["docker_file"] } down')
+    subprocess.run(['sh','-c',liquibase_command], check=True)
+    subprocess.run(['sh','-c',liquibase_down], check=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Execute different liquibase commands')
+    parser.add_argument('--execute', help='username for postgres')
+    args = parser.parse_args()
+
+    print('Execution starts --->>')
+    main(args)
